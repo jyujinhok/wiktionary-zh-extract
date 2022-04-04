6b5ef58ac0c53efafa55f2408582ea33270a1f90
Ben Yang <beyang@adobe.com>
2022-04-04 13:33:02 -0700

    added arguments to dial-to-json

--- dial-syn-parse/dial-to-json.py
+++ dial-syn-parse/dial-to-json.py
@@ -3,29 +3,36 @@ import xml.etree.ElementTree as ET
 import sys
 import subprocess
 import json
+import argparse
 
 PREFIX = "Module:zh/data/dial-syn/"
-tree = ET.parse("modules.xml")
 
-root = tree.getroot()
+parser = argparse.ArgumentParser()
+parser.add_argument("input", nargs="?", default="./modules.xml", help="modules.xml file from dump-extract (default './modules.xml'")
 
-out = {}
+if __name__ == "__main__":
+    args = parser.parse_args()
+    tree = ET.parse(args.input)
 
-for child in tqdm(root):
-    title = child.find('title').text
-    if title.startswith(PREFIX):
-        title = title[len(PREFIX):]
+    root = tree.getroot()
 
-    body = child.find("text")
-    with open("moduletest.lua", "w") as module:
-        module.write(body.text)
-    run = subprocess.run(["lua", "runner.lua"], capture_output = True)
-    if run.returncode != 0:
-        print("Skipping", title)
-        continue
-    # print(run.stdout)
-    out[title] = json.loads(run.stdout)
+    out = {}
 
-with open("dial.json", "w") as outfile:
-    json.dump(out, outfile, ensure_ascii = False)
-print("Written to dial.json")
+    for child in tqdm(root):
+        title = child.find('title').text
+        if title.startswith(PREFIX):
+            title = title[len(PREFIX):]
+
+        body = child.find("text")
+        with open("moduletest.lua", "w") as module:
+            module.write(body.text)
+        run = subprocess.run(["lua", "runner.lua"], capture_output = True)
+        if run.returncode != 0:
+            print("Skipping", title)
+            continue
+        # print(run.stdout)
+        out[title] = json.loads(run.stdout)
+
+    with open("dial.json", "w") as outfile:
+        json.dump(out, outfile, ensure_ascii = False)
+    print("Written to dial.json")
